ed78d386cea54384d152b75743cef5325b7f3248
xxxguoxinxxx <49920095+xxxguoxinxxx@users.noreply.github.com>
2023-06-26 22:10:59 +0800

    Add files via upload

---
+++ src/utils/contract.js
@@ -0,0 +1,128 @@
+import { ethers } from 'ethers'
+import abi from './abi.json'
+import { Web3 } from 'web3'
+import axios from '@/services/request'
+
+const BuyNFT = {
+  address:'0xD01fd148cb0e735e3b41a4F0d4940277Bf428b11',
+  'abi':abi
+}
+
+//stake and claim
+const Stake = {
+  address: '0xf5A36FF8A4c191bEa4054A459D1dbFe90F41EB1C',
+  'abi': abi
+}
+
+const USDT = {
+  address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+  'abi': abi
+}
+
+const NFT = {
+  address: '0xc934F270079741fB66F19e1CF16267078C5a8394',
+  'abi': abi
+}
+
+const AppoveUsdt = async() => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    USDT.address,
+    USDT.abi,
+    signer
+  )
+  var res =  await c.approve(BuyNFT.address,9900000)
+  return await res.wait()
+}
+
+const AppoveNFT = async() => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    NFT.address,
+    NFT.abi,
+    signer
+  )
+  var res =  await c.setApprovalForAll(Stake.address,true)
+  return await res.wait()
+}
+
+
+const Buy = async() => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    BuyNFT.address,
+    BuyNFT.abi,
+    signer
+  )
+  var res =  await c.exchangeNft(1,1)
+  return await res.wait()
+}
+
+
+const StakeNFT = async() => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    Stake.address,
+    Stake.abi,
+    signer
+  )
+  var res =  await c.stakeNFT('03125f0798586755da7976cb34bd4f959afa790db8eae632323110857d232e3dee','03933d1cf714b6302b834c05ecf25dd69838f94e5dca5fd764883ca2772f3dafd9',1)
+  return await res.wait()
+}
+
+const claimToken = async() => {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    Stake.address,
+    Stake.abi,
+    signer
+  )
+  var res =  await c.claimBGT()
+  return await res.wait()
+}
+
+
+
+
+const claimToken11 = async() => {
+  // await window.ethereum.enable()
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const c = new ethers.Contract(
+    Financial.address,
+    Financial.abi,
+    signer
+  )
+  var res =  await c.release(2)
+  return await res.wait()
+}
+
+
+
+const connectToWallet = async () =>{
+  var res =  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  if (!window.ethereum) return alert('请连接钱包')
+  if (ethereum.networkVersion != 137) return alert('请链接Polygon')
+  // if (ethereum.networkVersion != 137 && ethereum.networkVersion != 137) return 1
+  else return res
+}
+export {
+  AppoveUsdt,
+  AppoveNFT,
+  Buy,
+  StakeNFT,
+  claimToken,
+  connectToWallet,
+  claimToken11,
+}
---
+++ src/views/index/index.vue
@@ -0,0 +1,225 @@
+<template>
+
+  <div>
+    <van-sticky>
+      <Uheader/>
+    </van-sticky>
+    <div><br><br></div>
+    <van-button type="primary" @click="AppoveUsdt" v-if="show">授权USDT</van-button>
+    <br><br>
+    <van-button type="primary" @click="BuyNFT" v-if="show">买NFT</van-button>
+
+    <br><br>
+    <van-button type="primary" @click="AppoveNFT" v-if="show">授权NFT</van-button>
+    
+    <br><br>
+    <van-button type="primary" @click="StakeNFT" v-if="show">质押NFT</van-button>
+    <br><br>
+
+    <van-button type="primary" @click="claimToken" v-if="show">领取BGT</van-button>
+  </div>    
+    
+
+</template>
+<script>
+import {Toast,Dialog, Notify,ImagePreview,Button,Popup} from 'vant';
+
+import Uheader from '@/components/Uheader'
+
+// import Card from './component/card.vue'
+import {claimToken11,connectToWallet, AppoveUsdt,AppoveNFT, Buy, StakeNFT,claimToken,} from '@/utils/contract'
+// import {setWallet , getWallet} from '@/utils/auth'
+import {Slide} from 'vue-burger-menu'
+
+
+// import '~antd/dist/antd.css';
+
+
+export default {
+  components:{Uheader,Slide,Toast,Dialog, Notify,ImagePreview,[Button.name]: Button,Popup},
+  data() {
+    return {
+      show:true,
+    };
+  }, async mounted() {
+    const res = await connectToWallet()
+    if(res == 1 ) {
+      Toast('请链接BSC网络');
+      this.show = false
+    }
+  },
+  computed:{
+  },
+  methods:{
+
+    async AppoveNFT(){
+      await AppoveNFT()
+    },
+    async AppoveUsdt(){
+      await AppoveUsdt()
+    },
+    async BuyNFT(){
+      await Buy()
+    },
+    async StakeNFT(){
+      await StakeNFT()
+    },
+    async claimToken(){
+      await claimToken()
+    }
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.test{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.title{
+    width: 100%;
+    min-height: 24px;
+    font-size: 24px;
+    font-family: Microsoft YaHei;
+    font-weight: bold;
+    color: #000000; 
+    line-height: 26px;
+    // margin-left: 0.9rem;
+}
+.wen{
+    width: 90%;
+    min-height: 27px;
+    font-size: 10px;
+    font-family: Microsoft YaHei;
+    font-weight: 400;
+    color: #000000;
+    line-height: 1.6;
+    margin-top: 0.9rem;
+    margin-left: 1.2rem;
+    text-align: left;
+}
+.btn_partner{
+    height: 32px;
+    border-radius: 10px;
+    min-width: 106px;
+    line-height: 32px;
+    margin-top: 0.9rem;
+    font-size: 13px;
+    border:0px;
+    background-color: green;
+    color: #FFFFFF;
+    font-weight: bold;
+    font-family: 'Microsoft YaHei';
+}
+.ban{
+    margin: auto;
+    margin-top: 2rem;
+    height:4rem;
+    line-height:  4rem;
+    width: 90%;
+    border-radius: 0.5rem;
+    // background-image: linear-gradient(#2EB6BE, #285BA9);
+    background-image: linear-gradient(to right,#1989fa , rgb(4, 99, 172)); 
+}
+.bankuai{
+    font-size: 1.3rem;
+    font-family: Microsoft YaHei;
+    font-weight: bold;
+    color: #FFFFFF;
+}
+.outer_box{
+    margin-top: 1rem;
+    width:100%;
+}
+.box{
+    margin:auto;
+    display: flex;
+    width:90%;
+    min-height:8.5rem;
+    background-color: #ecf9ff;
+    border-radius: 1.15rem;
+}
+.left_box{
+    display: flex;
+    flex:5;
+    min-height:8.5rem;
+    align-items: center;
+    justify-content: center;
+}
+.left_img{
+    /* align-items: center; */
+    // width:4.5rem;
+    width: 55px;
+    /* height: 4.5rem; */
+}
+.right_box{
+    display:flex;
+    flex:15;
+    flex-direction:column;
+    min-height:9.0rem;
+    padding-bottom: 1rem;
+    padding-right: 1rem;
+    /* background-color: red; */
+    text-align: left;
+   
+}
+.right_box .right_top{
+    flex:1;
+    margin-left: 0.9rem;
+    margin-top: 0.9rem;
+    font-size: 13px;
+    font-family: 'Microsoft YaHei';
+    font-weight: bold;
+   
+    /* height:1rem; */
+    /* background-color: yellow; */
+}
+.right_bottom{
+    font-size: 10.5px;
+    font-family: 'Microsoft YaHei';
+    margin-top: 0.1rem;
+    margin-left: 0.9rem;
+    /* height:1rem; */
+    flex:5;
+}
+.box_100{
+    margin-top: 1rem;
+    width:100%;
+    min-height:3rem;
+}
+.box_90{
+    margin: auto;
+    width:90%;
+    min-height:3rem;
+    background-color: #ecf9ff;
+    border-radius: 32px;
+    display: flex;
+    align-items: center;
+    padding-right: 1rem;
+}
+.num{
+ flex:4;
+ font-size: 18px;
+ font-family: Microsoft YaHei;
+ font-weight: bold;
+ text-align: left;
+//  background-image: linear-gradient(to right,#285AA9 , #2FBFBF);
+ 
+}
+.num span{
+    // background-image: linear-gradient(to right,#285AA9 , #2FBFBF);
+  background-image: linear-gradient(to right,#1989fa , green); 
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  margin-left: 1rem; 
+}
+.num_rule{
+    text-align: left;
+    flex:10;
+    margin-left: 2rem; 
+}
+</style>
